bc4916d20988f1ded50c033f32e05ac9f372b7f8
Swayam1507 <s2m0i0t3@gmail.com>
2022-06-07 13:20:17 -0700

    Updates

---
+++ precache-manifest.bd6d32d2f1d0314f3fd920c787f86ed3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0080c573440f190f124",
+    "url": "/textutils/static/css/main.9fef34e0.chunk.css"
+  },
+  {
+    "revision": "c0080c573440f190f124",
+    "url": "/textutils/static/js/main.c0080c57.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/textutils/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "20c08009ca8527c6c31c",
+    "url": "/textutils/static/js/2.20c08009.chunk.js"
+  },
+  {
+    "revision": "8e45b1fa04ebdfd146c8",
+    "url": "/textutils/static/js/runtime~main.8e45b1fa.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/textutils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c8c0b9178449e5ae845ef65842244129",
+    "url": "/textutils/index.html"
+  }
+];
